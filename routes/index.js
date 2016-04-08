6c0439f33977217212fe3ab31794cc840972dd6f
Ean McLaughlin <ean.mclaughlin@rocky.edu>
2016-04-08 09:32:09 -0600

    Added database module dependency and example usage.

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var database = require('frontier-database')();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('manual_data_form', {id: 1});
 });
 
-
 router.get('/room', function (req, res, next) {
-    res.render('new_room_form', {id: 1});
 });
 
 router.post('/room', function (req, res, next) {
-    res.send("Hi");
 });
+
 router.get('/sensor', function (req, res, next) {
-    res.render('new_sensor_form', {
-        rooms: [
-            {
-                id:1,
-                name:"First"
-            },
-            {
-                id:2,
-                name:"Second"
-            }
-        ]
-    });
+    database.getRooms({}, "_id name", null).then(function (rooms) {
+        res.render('new_sensor_form', {
+            rooms: rooms
+        });
+    })
 });
 
 module.exports = router;
